ecbad2c7bd3ed16301145861cc367bb6cbd59d3a
Samuel Monção <samu.henr@gmail.com>
2026-03-15 21:10:01 -0300

    fix: Ajuste de import

--- src/pages/students/StudentsPage.tsx
+++ src/pages/students/StudentsPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import axios from 'axios'
 import {
   BarChartOutlined,
